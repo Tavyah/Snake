7a9e99c483efd7a02c7da7fc9e34e9191761b93d
Tavyah <94623622+Tavyah@users.noreply.github.com>
2024-11-28 13:22:55 +0100

    snake sprite will rotate head towards correct direction

--- Scenes/snake.gd
+++ Scenes/snake.gd
@@ -4,7 +4,7 @@ signal hit
 @export var speed = 50
 var screen_size
 var velocity = Vector2.ZERO
-var current_input
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -16,15 +16,19 @@ func check_movement(delta: float) -> void:
 	# Checking if velocity is 0 or not, if not 0, then a direction is already set on same index (x,y). If not a direction set in same index, then u can set a direction
 	# Don't want the snake to be able to move towards it's own body.
 	if Input.is_action_just_pressed("move_up") and velocity.y == 0:
+		sprite_2d.set_rotation_degrees(0)
 		velocity = Vector2.ZERO
 		velocity.y -= 1
 	if Input.is_action_just_pressed("move_down") and velocity.y == 0:
+		sprite_2d.set_rotation_degrees(180)
 		velocity = Vector2.ZERO
 		velocity.y += 1
 	if Input.is_action_just_pressed("move_right") and velocity.x == 0:
+		sprite_2d.set_rotation_degrees(90)
 		velocity = Vector2.ZERO
 		velocity.x += 1
 	if Input.is_action_just_pressed("move_left") and velocity.x == 0:
+		sprite_2d.set_rotation_degrees(-90)
 		velocity = Vector2.ZERO
 		velocity.x -= 1
 		
